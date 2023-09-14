bce6d677b03127830f060ad6f7151c1266aa3503
daylaviet <vietnetk123@gmail.com>
2023-09-14 16:36:59 +0700

    add initial profile

--- src/app/(setup)/page.tsx
+++ src/app/(setup)/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+
+export default async function SetupPage() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) return redirect(`server/${server.id}`);
+
   return <div> Create</div>;
 }
