adec61b7194b947fdb5812534705d2f238abe02b
daylaviet <vietnetk123@gmail.com>
2023-09-22 21:03:43 +0700

    add server search modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ModalProvider } from "@/components/providers/modal-provider";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={font.className} suppressHydrationWarning>
+        <body
+          className={cn(font.className, "bg-white dark:bg-[#313338]")}
+          suppressHydrationWarning
+        >
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
