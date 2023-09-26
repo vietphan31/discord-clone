c3dd169096db1d4feeffffcf1039b2441ed3f35e
daylaviet <vietnetk123@gmail.com>
2023-09-26 22:20:06 +0700

    add messages

--- src/components/providers/modal-provider.tsx
+++ src/components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import { InviteModal } from "@/components//modals/invite-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
+import { DeleteMessageModal } from "@/components/modals/delete-message-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
@@ -33,6 +34,7 @@ export const ModalProvider = () => {
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
+      <DeleteMessageModal />
     </>
   );
 };
